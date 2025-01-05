a3881f52539b0fd431f79d8ed6ce67913fbdb13c
P-pisiko <enesfidan75@proton.me>
2025-01-05 18:02:34 -0500

    Add project files.

---
+++ FileTraversel/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace FileTraversel
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            string rootDirectory = @"C:\Users\T-Box";
+
+            string[] fileExtensions = { ".png", ".old",".mp4" };
+
+            string[] excludedDirectories = { "AppData", "Application Data" };
+
+            var files = new List<string>();
+
+            GetFiles(rootDirectory, fileExtensions, excludedDirectories, files);
+
+            ProcessFiles(files.ToArray());
+
+            Console.WriteLine("Press something to close the application. .");
+            Console.ReadLine();
+        }
+
+        static void GetFiles(string directory, string[] fileExtensions, string[] excludedDirectories, List<string> files)
+        {
+            try
+            {
+                // Check if the current directory is in the excluded list
+                if (!excludedDirectories.Any(excludedDirectory => directory.EndsWith(excludedDirectory, StringComparison.OrdinalIgnoreCase)))
+                {
+                    try
+                    {
+                        // Get all files in the current directory
+                        foreach (string file in Directory.EnumerateFiles(directory, "*.*"))
+                        {
+                            // Check file extension
+                            if (fileExtensions.Any(extension =>
+                                file.EndsWith(extension, StringComparison.OrdinalIgnoreCase)))
+                            {
+                                files.Add(file);
+                            }
+                        }
+                    }
+                    
+                    catch (Exception ex)
+                    {
+                    }
+
+                    // Traverse subdirectories
+                    try
+                    {
+                        foreach (string subdirectory in Directory.EnumerateDirectories(directory))
+                        {
+                            GetFiles(subdirectory, fileExtensions, excludedDirectories, files);
+                        }
+                    }
+                    
+                    catch (Exception ex)
+                    {
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+            }
+        }
+
+        static void ProcessFiles(string[] files)
+        {
+            foreach (string file in files)
+            {
+                Console.WriteLine($"Processing file: {file}");
+            }
+        }
+    }
+}
---
+++ FileTraversel/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("FileTraversel")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("FileTraversel")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("36817ddc-52b2-41fd-a6f9-8426f4f99e67")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
